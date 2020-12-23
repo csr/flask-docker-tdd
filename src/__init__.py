b992baec5536bdd7d987ba12309bf096fdf354a7
Cesare De Cal <hi@cesare.io>
2020-12-23 21:15:50 +0100

    Loading settings from Docker

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 # src/__init__.py
 
+import os
 
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
@@ -11,7 +12,8 @@ app = Flask(__name__)
 api = Api(app)
 
 # Set config
-app.config.from_object('src.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 class Ping(Resource):
