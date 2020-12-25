8f63d1ec8f375d7c7dac194c41ae2ceea7fc56c8
Cesare De Cal <hi@cesare.io>
2020-12-25 11:31:31 +0100

    Set up Flask blueprints

--- manage.py
+++ manage.py
@@ -3,13 +3,15 @@
 
 from flask.cli import FlaskGroup
 
-from src import app, db
+from src import create_app, db
+from src.api.models import User
 
 
-cli = FlaskGroup(app)
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 
-@cli.command('recreate.db')
+@cli.command('recreate_db')
 def recreate_db():
     db.drop_all()
     db.create_all()
--- src/__init__.py
+++ src/__init__.py
@@ -2,42 +2,33 @@
 
 import os
 
-from flask import Flask, jsonify
-from flask_restx import Resource, Api
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-# Instantiate the app
-app = Flask(__name__)
-
-api = Api(app)
-
-# Set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
 # instantiate the db
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-# model
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app(script_info=None):
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    # Instantiate the app
+    app = Flask(__name__)
 
+    # Set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-class Ping(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+    # Set up extensions
+    db.init_app(app)
 
-api.add_resource(Ping, '/ping')
+    # Register blueprints
+    from src.api.ping import ping_blueprint
+    app.register_blueprint(ping_blueprint)
+
+    # Shell context for flask CLI
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
